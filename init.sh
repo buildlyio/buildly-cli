b3a60f46b6839533a2514613a9ba7be1dc977e31
Greg Lind <gwlind@gmail.com>
2019-08-27 07:48:13 -0400

    update to use hub TODO: fix library import

--- init.sh
+++ init.sh
@@ -10,21 +10,43 @@ if [ "$answer" != "${answer#[Ss]}" ] ;then
 
     echo -n "Would you like to Manage Users with Buildly? Yes [Y/y] or No [N/n]"
     read users
+    
+    # cp config file to make changes
+    cp buildly/bifrost-api/settings/base.py buildly/bifrost-api/settings/base-buildly.py
 
     if [ "$users" != "${users#[Nn]}" ] ;then
-        sed 's/users//g' buildly/bifrost-api/settings/base.py > buildly/bifrost-api/settings/base-buildly.py
+        sed 's/users//g' buildly/bifrost-api/settings/base-buildly.py > buildly/bifrost-api/settings/base-buildly.py
     fi
 
     echo -n "Would you like to use Templates to manage reuseable workflows with Buildly? Yes [Y/y] or No [N/n]"
     read templates
     
     if [ "$templates" != "${templates#[Nn]}" ] ;then
-        sed 's/templates//g' buildly/bifrost-api/settings/base.py > buildly/bifrost-api/settings/base-buildly.py
+        sed 's/workflow//g' buildly/bifrost-api/settings/base-buildly.py > buildly/bifrost-api/settings/base-buildly.py
     fi
     echo -n "Would you like to enable the data mesh functions? Yes [Y/y] or No [N/n]"
     read mesh
 
     if [ "$mesh" != "${mesh#[Nn]}" ] ;then
-        sed 's/mesh//g' buildly/bifrost-api/settings/base.py > buildly/bifrost-api/settings/base-buildly.py
+        sed 's/datamesh//g' buildly/bifrost-api/settings/base-buildly.py > buildly/bifrost-api/settings/base-buildly.py
     fi
 fi
+
+# create new repo
+if ! [ -x "$(command -v hub)" ]; then    
+    git clone \
+    --config transfer.fsckobjects=false \
+    --config receive.fsckobjects=false \
+    --config fetch.fsckobjects=false \
+    https://github.com/github/hub.git
+
+    cd hub
+    make install prefix=/usr/local
+fi
+cd ../buildly
+rm -Rf .git
+git init
+git add -A .
+git commit -m "a new buildly repo init"
+# init remote in users repo
+hub init -g
