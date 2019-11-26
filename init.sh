5c1c97647083b4c1b2e32aebbed5022518750dab
Jeferson Moura <jefmmoreira@gmail.com>
2019-11-26 20:29:20 +0100

    Update setupService method to build Buildly Core locally

--- init.sh
+++ init.sh
@@ -273,31 +273,41 @@ setupServices()
   # Check specific dependencies
   type docker >/dev/null 2>&1 || { echo >&2 "ERROR: You do not have 'Docker' installed.
   Check the documentation of how to install it: https://docs.docker.com/v17.12/install/"; exit 1; }
-  type kubectl >/dev/null 2>&1 || { echo >&2 "ERROR: You do not have 'K8S CLI' installed.
-  Check the documentation of how to install it: https://kubernetes.io/docs/tasks/tools/install-kubectl/"; exit 1; }
 
-  # check if service folder exists inside of application's folder
-  if [ ! -d YourApplication/services ]; then
-    MSG="The application folder \"YourApplication/services\" doesn't exist"
-    print_message "error" "$MSG"
-  fi
-
-  (
-  cd "YourApplication/services" || return
   eval $(minikube docker-env)
-  # loop through all services and build their images
-  ls | while IFS= read -r service
-  do
+  if [[ -n "$1" && ("$1" == "buildly") ]] ;then
+    # check if buildly core folder exists inside of application's folder
+    if [ ! -d YourApplication/buildly-core ]; then
+      MSG="The application folder \"YourApplication/buildly-core\" doesn't exist"
+      print_message "error" "$MSG"
+    fi
+
+    # build buildly core
     (
-    cd $service || exit
-    cleanedService=$(echo "$service" | tr "[:punct:]" -)
-    # build a local image
-    docker build . -t "${cleanedService}:latest" || exit
-    # deploy to kubectl
-    kubectl run $cleanedService --image=$cleanedService --image-pull-policy=Never -n buildly
+    cd YourApplication/buildly-core || return
+    docker build . -t "buildly-core:latest" || exit
     )
-  done
-  )
+  else
+    # check if service folder exists inside of application's folder
+    if [ ! -d YourApplication/services ]; then
+      MSG="The application folder \"YourApplication/services\" doesn't exist"
+      print_message "error" "$MSG"
+    fi
+
+    (
+    cd "YourApplication/services" || return
+    # loop through all services and build their images
+    ls | while IFS= read -r service
+    do
+      (
+      cd $service || exit
+      cleanedService=$(echo "$service" | tr "[:punct:]" -)
+      # build a local image
+      docker build . -t "${cleanedService}:latest" || exit
+      )
+    done
+    )
+  fi
 }
 
 ##############################################################################
